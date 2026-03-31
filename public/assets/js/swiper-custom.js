25ad2dcc7d505ca2ddf8d3e5ca3cb1eb00f79470
dikshita <dikshita@techiedom.com>
2026-04-01 03:04:46 +0530

    Added process sec img JS

--- public/assets/js/swiper-custom.js
+++ public/assets/js/swiper-custom.js
@@ -1,13 +1,33 @@
 var swiper = new Swiper(".team-swiper", {
-  loop: true,
+    loop: true,
 
-  autoplay: {
-    delay: 4800,
-    disableOnInteraction: false,
-  },
+    autoplay: {
+        delay: 4800,
+        disableOnInteraction: false,
+    },
 
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
+
+// process sticky box
+$(window).scroll(function () {
+    var scrollTop = $(window).scrollTop();
+
+    var sectionTop = $(".operation-sec").offset().top;
+    var sectionHeight = $(".operation-sec").outerHeight();
+
+    var stickyHeight = $(".sticky-box").outerHeight();
+
+    var sectionBottom = sectionTop + sectionHeight - stickyHeight;
+
+    if (scrollTop > sectionTop && scrollTop < sectionBottom) {
+        $(".sticky-box").removeClass("stop").addClass("sticky");
+    } else if (scrollTop > sectionBottom) {
+        $(".sticky-box").removeClass("sticky").addClass("stop");
+    } else {
+        $(".sticky-box").removeClass("sticky stop");
+    }
 });
